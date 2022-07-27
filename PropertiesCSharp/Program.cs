ba12a5b43de7ff7966168357b7938761403f9ffb
libyano93ly <younus93ly@gmail.com>
2022-07-27 13:25:40 +0200

    Update 1

--- PropertiesCSharp/Program.cs
+++ PropertiesCSharp/Program.cs
@@ -6,7 +6,7 @@ namespace PropertiesCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldd!");
         }
     }
 }
